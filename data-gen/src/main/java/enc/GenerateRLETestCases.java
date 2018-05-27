3d83d425fae0f896e70076c62825b06657c3b913
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-05-27 12:19:52 +0100

    data-gen: update to use parquet 1.10.0

--- data-gen/src/main/java/enc/GenerateRLETestCases.java
+++ data-gen/src/main/java/enc/GenerateRLETestCases.java
@@ -4,9 +4,12 @@ import static java.lang.System.out;
 
 import java.io.IOException;
 
+import org.apache.parquet.bytes.HeapByteBufferAllocator;
 import org.apache.parquet.column.values.rle.RunLengthBitPackingHybridEncoder;
 
 public class GenerateRLETestCases {
+    private static final HeapByteBufferAllocator A = new HeapByteBufferAllocator();
+
     private static void printLine() {
         out.println("===============================================\n");
     }
@@ -21,8 +24,7 @@ public class GenerateRLETestCases {
 
     private static void genSingleRLERun_10_Zeroes_1_bit() throws IOException {
         out.println("Single RLE run: 1-bit per value, 10x0");
-        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(
-                1, 10, 10000);
+        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(1, 10, 10000, A);
         for (int i = 0; i < 10; i++) {
             e.writeInt(0);
         }
@@ -32,8 +34,7 @@ public class GenerateRLETestCases {
 
     private static void genSingleRLERun_300_Ones_20_bit() throws IOException {
         out.println("Single RLE run: 20-bits per value, 300x1");
-        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(
-                20, 10, 10000);
+        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(20, 10, 10000, A);
         for (int i = 0; i < 300; i++) {
             e.writeInt(1);
         }
@@ -43,8 +44,7 @@ public class GenerateRLETestCases {
 
     private static void gen2RLERuns_10_Zeroes_9_Ones_1_bit() throws IOException {
         out.println("2 RLE runs: 1-bit per value, 10x0, 9x1");
-        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(
-                1, 10, 10000);
+        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(1, 10, 10000, A);
         for (int i = 0; i < 10; i++) {
             e.writeInt(0);
         }
@@ -57,8 +57,7 @@ public class GenerateRLETestCases {
 
     private static void genSingleBitPackedRun_0to7_3bit() throws IOException {
         out.println("1 bit-packed run: 3 bits per value, 0,1,2,3,4,5,6,7");
-        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(
-                3, 10, 10000);
+        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(3, 10, 10000, A);
         for (int i = 0; i <= 7; i++) {
             e.writeInt(i);
         }
@@ -68,8 +67,7 @@ public class GenerateRLETestCases {
 
     private static void genRLE_BitPacked_RLE_2bit() throws IOException {
         out.println("RLE run, bit packed run, RLE run: 8x1, 0, 1, 2, 3, 3, 2, 1, 0, 10x2");
-        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(
-                2, 10, 10000);
+        RunLengthBitPackingHybridEncoder e = new RunLengthBitPackingHybridEncoder(2, 10, 10000, A);
         e.writeInt(1);
         e.writeInt(1);
         e.writeInt(1);
@@ -110,4 +108,4 @@ public class GenerateRLETestCases {
         genSingleBitPackedRun_0to7_3bit();
         genRLE_BitPacked_RLE_2bit();
     }
-}
+} 
