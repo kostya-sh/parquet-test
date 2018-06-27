0a7c8733c372c10d9e8daf1a9df05a65356182ab
Konstantin Shaposhnikov <k.shaposhnikov@gmail.com>
2018-06-27 19:08:28 +0100

    data-gen: generator for DELTA and DELTA_LENGTH encodings tests

--- data-gen/src/main/java/enc/GenerateByteArrayTestCases.java
+++ data-gen/src/main/java/enc/GenerateByteArrayTestCases.java
@@ -5,6 +5,8 @@ import static java.lang.System.out;
 import java.io.IOException;
 
 import org.apache.parquet.bytes.HeapByteBufferAllocator;
+import org.apache.parquet.column.values.deltalengthbytearray.DeltaLengthByteArrayValuesWriter;
+import org.apache.parquet.column.values.deltastrings.DeltaByteArrayWriter;
 import org.apache.parquet.column.values.plain.PlainValuesWriter;
 import org.apache.parquet.io.api.Binary;
 
@@ -36,7 +38,37 @@ public class GenerateByteArrayTestCases {
         printLine();
     }
 
+    private static void genDeltaLengthByteArrays(String... values) throws IOException {
+        DeltaLengthByteArrayValuesWriter w = new DeltaLengthByteArrayValuesWriter(10000, 10000,  A);
+        
+        for (String s: values) {
+        	out.print("[]byte(\"" + s + "\"), ");
+        	w.writeBytes(Binary.fromConstantByteArray(s.getBytes()));
+        }
+        out.println();
+
+        printBytes(w.getBytes().toByteArray());
+        printLine();
+    }
+
+    private static void genDeltaByteArrays(String... values) throws IOException {
+        DeltaByteArrayWriter w = new DeltaByteArrayWriter(10000, 10000,  A);
+        
+        for (String s: values) {
+        	out.print("[]byte(\"" + s + "\"), ");
+        	w.writeBytes(Binary.fromConstantByteArray(s.getBytes()));
+        }
+        out.println();
+
+        printBytes(w.getBytes().toByteArray());
+        printLine();
+    }
+    
     public static void main(String[] args) throws IOException {
     	genMiscByteArrays();
+    	
+     	genDeltaLengthByteArrays("Hello", "World", "Foobar", "ABCDEF", "");
+    
+     	genDeltaByteArrays("", "ABC", "ABCDEF", "Hello", "World", "");
     }
 } 
